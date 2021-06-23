230145ffc0592d5123bd875d0c61dc793ff06713
Lenis Pardo <len.pardo@duocuc.cl>
2021-06-22 22:56:41 -0400

    actualizacion conf django

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import pagina
+from .views import home,sismos,informacion,productos,contacto
 
 urlpatterns = [
- path('', pagina,name="pagina")
+ path('', home,name="Pagina"),
+ path('/sismos', sismos,name="Sismos"),
+ path('/informacion', informacion,name="Informacion"),
+ path('/productos', productos,name="Productos"),
+ path('/contacto', contacto,name="Contacto"),
+
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def pagina(request):
+def home(request):
   return render(request,'core/pagina.html')
 
+def sismos(request):
+  return render(request,'core/sismos.html')
+
+def informacion(request):
+  return render(request,'core/quienesSomos.html')
+
+def productos(request):
+  return render(request,'core/nuestrosproductos.html')
+
+def contacto(request):
+  return render(request,'core/contactanos.html')
