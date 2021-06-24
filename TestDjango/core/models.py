d51018b899fd9b26be7910b4d8e98fba084415a1
Lenis Pardo <len.pardo@duocuc.cl>
2021-06-24 02:20:29 -0400

    tabla bd agregada

--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -3,16 +3,12 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     identificacion = models.IntegerField(primary_key=True, verbose_name="Id de Usario")
-    telefono = models.classEmailField ( max_length=50, verbose_name="Nombre de la categoria" ) 
-    direccion =models.CharField(max_length=50, verbose_name=name="direccion")
-    nombreCategoria = models.CharField(max_length=50, verbose_name="Nombre de la categoria")
-
+    telefono = models.CharField ( max_length=10, verbose_name="telefono" ) 
+    email =models.classEmailField(max_length=50, verbose_name=name="email")
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    email = models.CharField(max_length=50, verbose_name="email")
+    constraseña = models.CharField(max_length=50, verbose_name="Constraseña")
+    pais = models.CharField(max_length=50, verbose_name="pais")
 
     def __str__(self):
-        return self.nombreCategoria
-
-        Número de identificación
-Dirección.
-Email
-Contraseña
-País.
+        return self.telefono
