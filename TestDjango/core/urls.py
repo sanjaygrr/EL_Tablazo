94744104dce1a51a94466a7d4dc002f19dafc9fc
sanjaygrr <65647041+sanjaygrr@users.noreply.github.com>
2021-06-28 02:50:25 +0530

    crees unos archivos y cambie el logo
    puse dos archivos nuevos para crear la entrada de informacion para guardar en el backend

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
  path('informacion', informacion,name="informacion"),
  path('productos', productos,name="productos"),
  path('contacto', contacto,name="contacto"),
+ path('cliente_formulario', cliente_formulario,name="cliente_formulario"),
+ path('clientes', clientes,name="clientes"),
+ path('CLiente', Cliente,name="Cliente"),
 ]
